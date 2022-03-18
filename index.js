9ea04886366731d950d2e4823667c98b64adb42c
Isabella Costabile <costabileisa@gmail.com>
2022-03-18 13:31:21 -0400

    commit tree d1b86966c3feab7cd90bc2dc2039c8f06c5d00ed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+let employee = {};
+function reset() {
+    for (const key in employee) {
+        delete employee[key];
+      }
+      employee.name = 'Sam';
+}
+
+reset();
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+reset();
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+reset();
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee
+}
+
+reset();
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
